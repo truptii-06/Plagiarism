88416c997a56fdb7975b80495740e051ec3c59aa
Vrushali Gawai <vrushalig2004@gmail.comgit config --global user.email vrushalig2004@gmail.com>
2025-09-11 13:09:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Homepage from './components/Homepage/Homepage'
+import Loginpage from './components/Loginpage/Loginpage'
+const App = () => {
+  return (
+    <>
+      <Homepage/>
+      <Loginpage/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Loginpage/Loginpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loginpage = () => {
+  return (
+    <div>
+      This is Login page
+    </div>
+  )
+}
+
+export default Loginpage
